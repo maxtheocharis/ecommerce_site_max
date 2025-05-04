b1d5472c137eb3dc4288702ac29ca2b8258b018c
max theocharis <maxteo1985@gmail.com>
2025-05-04 12:51:27 +0300

    sto views allaxa to render

--- ecommerce/catalog/views.py
+++ ecommerce/catalog/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from .models import Product
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'myapp/index.html', {'products': products})
+    return render(request, 'catalog/index.html', {'products': products})
 def product_detail(request, pk):
     product = Product.objects.get(pk=pk)
-    return render(request, 'myapp/index2.html', {'product': product})
+    return return render(request, 'catalog/index1.html', {'product': product})
 from django.http import HttpResponse
 def home(request):
     return HttpResponse('Hello, World!')
